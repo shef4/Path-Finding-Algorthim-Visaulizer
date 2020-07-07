fbe50bb1c3fe33c3202b71058e18aac73919dc97
Shef <sefunmi88@gmail.com>
2020-07-07 01:08:55 -0400

    added files

---
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+
+import './PathfindingVisualizer.css';
+
+export default class PathfindingVisualizer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nodes: [], 
+        };
+    }
+
+    componentDidMount(){
+        const nodes = [];
+        for (let row = 0; row < 15; row++) {
+            const currentRow = [];
+            for (let col = 0; col < 50; col++) {
+                currentRow.push([]);            
+            }
+            nodes.push(currentRow);
+        }
+        this.setState({nodes})
+    }
+
+    render() {
+        const {nodes} = this.state;
+        console.log(nodes)
+
+        return (
+            <div classname="grid">
+                {nodes.map((row, rowIdx) => {
+                    return <div>
+                        {row.map((node, nodeIdx) => <Node></Node>)}
+                    </div>
+                })}            
+            </div>
+        );
+    }
+}
